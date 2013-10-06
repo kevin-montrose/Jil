6087641958e7e583e177778a07148d508f333914
kevin-montrose <kevin.montrose@gmail.com>
2013-10-06 14:04:17 -0400

    assembly info on experiments

--- Experiments/Properties/AssemblyInfo.cs
+++ Experiments/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Experiments")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Kevin Montrose 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
