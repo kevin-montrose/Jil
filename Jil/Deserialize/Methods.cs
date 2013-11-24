609012a764e07000566f72dcc78c475395da0108
kevin-montrose <kevin.montrose@gmail.com>
2013-11-24 16:09:07 -0500

    deserializing strings (ignoring null)

--- Jil/Deserialize/Methods.cs
+++ Jil/Deserialize/Methods.cs
@@ -11,6 +11,137 @@ namespace Jil.Deserialize
 {
     static class Methods
     {
+        public const int CharBufferSize = 4;
+
+        public static readonly MethodInfo ReadEncodedString = typeof(Methods).GetMethod("_ReadEncodedString", BindingFlags.Static | BindingFlags.NonPublic);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static string _ReadEncodedString(TextReader reader, char[] buffer, StringBuilder commonSb)
+        {
+            {
+                var ix = 0;
+
+                while (ix <= CharBufferSize)
+                {
+                    if (ix == CharBufferSize)
+                    {
+                        commonSb.Append(new string(buffer, 0, ix));
+                        break;
+                    }
+
+                    var first = reader.Read();
+                    if (first == -1) throw new DeserializationException("Expected any character");
+
+                    // we didn't have to use anything but the buffer, make a string and return it!
+                    if (first == '"')
+                    {
+                        return new string(buffer, 0, ix);
+                    }
+
+                    if (first != '\\')
+                    {
+                        buffer[ix] = (char)first;
+                        ix++;
+                        continue;
+                    }
+
+                    var second = reader.Read();
+                    if (second == -1) throw new DeserializationException("Expected any character");
+
+                    switch (second)
+                    {
+                        case '"': buffer[ix] = '"'; ix++; continue;
+                        case '\\': buffer[ix] = '\\'; ix++; continue;
+                        case '/': buffer[ix] = '/'; ix++; continue;
+                        case 'b': buffer[ix] = '\b'; ix++; continue;
+                        case 'f': buffer[ix] = '\f'; ix++; continue;
+                        case 'n': buffer[ix] = '\n'; ix++; continue;
+                        case 'r': buffer[ix] = '\r'; ix++; continue;
+                        case 't': buffer[ix] = '\t'; ix++; continue;
+                    }
+
+                    if (second != 'u') throw new DeserializationException("Unrecognized escape sequence");
+
+                    commonSb.Append(buffer, 0, ix);
+
+                    // now we're in an escape sequence, we expect 4 hex #s; always
+                    ix = 0;
+                    var read = 0;
+                    var toRead = 4;
+                    do
+                    {
+                        read = reader.Read(buffer, ix, toRead);
+                        if (read == 0) throw new DeserializationException("Expected characters");
+
+                        toRead -= read;
+                        ix += read;
+                    } while (toRead > 0);
+
+                    var c = FastHexToInt(buffer);
+                    commonSb.Append((char)c);
+                    break;
+                }
+            }
+
+            // fall through to using a StringBuilder
+
+            while (true)
+            {
+                var first = reader.Read();
+                if (first == -1) throw new DeserializationException("Expected any character");
+
+                if (first == '"')
+                {
+                    break;
+                }
+
+                if (first != '\\')
+                {
+                    commonSb.Append((char)first);
+                    continue;
+                }
+
+                var second = reader.Read();
+                if (second == -1) throw new DeserializationException("Expected any character");
+
+                switch (second)
+                {
+                    case '"': commonSb.Append('"'); continue;
+                    case '\\': commonSb.Append('\\'); continue;
+                    case '/': commonSb.Append('/'); continue;
+                    case 'b': commonSb.Append('\b'); continue;
+                    case 'f': commonSb.Append('\f'); continue;
+                    case 'n': commonSb.Append('\n'); continue;
+                    case 'r': commonSb.Append('\r'); continue;
+                    case 't': commonSb.Append('\t'); continue;
+                }
+
+                if (second != 'u') throw new DeserializationException("Unrecognized escape sequence");
+
+                // now we're in an escape sequence, we expect 4 hex #s; always
+                var ix = 0;
+                var read = 0;
+                var toRead = 4;
+                do
+                {
+                    read = reader.Read(buffer, ix, toRead);
+                    if (read == 0) throw new DeserializationException("Expected characters");
+
+                    toRead -= read;
+                    ix += read;
+                } while (toRead > 0);
+
+                var asInt = FastHexToInt(buffer);
+                commonSb.Append((char)asInt);
+            }
+
+            var ret = commonSb.ToString();
+
+            // leave this clean for the next use
+            commonSb.Clear();
+
+            return ret;
+        }
+
         public static readonly MethodInfo ReadEncodedChar = typeof(Methods).GetMethod("_ReadEncodedChar", BindingFlags.Static | BindingFlags.NonPublic);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static char _ReadEncodedChar(TextReader reader, char[] buffer)
@@ -51,9 +182,16 @@ namespace Jil.Deserialize
                 ix += read;
             } while (toRead > 0);
 
+
+            return (char)FastHexToInt(buffer);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static int FastHexToInt(char[] buffer)
+        {
             // TODO: this can be done much, much faster
             var asStr = new string(buffer, 0, 4);
-            return (char)int.Parse(asStr, System.Globalization.NumberStyles.HexNumber);
+            return int.Parse(asStr, System.Globalization.NumberStyles.HexNumber);
         }
     }
 }
