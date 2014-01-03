1195ca789590d4fb25ef3c6800ca6cfa96710f3f
kevin-montrose <kevin.montrose@gmail.com>
2014-01-02 23:38:45 -0500

    0.7.0

--- Jil/Properties/AssemblyInfo.cs
+++ Jil/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Jil")]
 [assembly: AssemblyDescription("A fast JSON serializer")]
 [assembly: AssemblyProduct("Jil")]
-[assembly: AssemblyCopyright("Copyright © Kevin Montrose 2013")]
+[assembly: AssemblyCopyright("Copyright © Kevin Montrose 2013 - 2014")]
 
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a4bd090c-cd3e-4f8e-af53-a947b0a87316")]
 
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0")]
 
 [assembly: InternalsVisibleTo("JilTests")]
 [assembly: InternalsVisibleTo("Experiments")]
