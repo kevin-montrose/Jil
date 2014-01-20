7d910eeb4963374def5b2ce9decf979d619ea253
kevin-montrose <kevin.montrose@gmail.com>
2014-01-19 19:24:38 -0500

    more anon tests

--- Jil/DeserializationException.cs
+++ Jil/DeserializationException.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Jil
 {
+    /// <summary>
+    /// An exception thrown when Jil encounters an error when deserializing a stream.
+    /// </summary>
     public class DeserializationException : Exception
     {
         internal DeserializationException(string msg) : base(msg) { }
