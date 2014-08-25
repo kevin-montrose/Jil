d400e2a56c237af2515d1e8e12fa390d8cd182f0
Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
2014-08-25 19:20:01 +1000

    Simplification refactoring
    Starting to split out the end of name functionality so I can handle
    multi-names, such as in the case of enums

--- Jil/Deserialize/NameAutomata.cs
+++ Jil/Deserialize/NameAutomata.cs
@@ -3,6 +3,7 @@ using Sigil;
 using Sigil.NonGeneric;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -105,6 +106,7 @@ namespace Jil.Deserialize
             }
         }
 
+        [DebuggerDisplay("{Name}")]
         public class AutomataName
         {
             public readonly string Name;
@@ -117,129 +119,90 @@ namespace Jil.Deserialize
             }
         }
 
-        static void Recurse(Data d, IList<AutomataName> nameValues, int pos, Label onMatchChar)
+        static void FinishName(Data d, AutomataName nameValue, Label onMatchChar)
         {
-            var i = 0;
+            d.AddAction(() => 
+            {
+                d.Emit.MarkLabel(onMatchChar);
+                nameValue.OnFound(d.Emit);
+                d.Emit.Return();
+            });
+        }
 
+        static void NextChar(Data d, IList<AutomataName> nameValues, int pos, Label onMatchChar)
+        {
             var chars =
                 nameValues
-                .Select(nv => pos >= nv.Name.Length ? -1 : nv.Name[pos])
-                .Distinct()
+                .GroupBy(nv => pos >= nv.Name.Length ? -1 : nv.Name[pos])
                 .ToList();
 
-            if (chars.Count == 1)
+            var namesToFinish =
+                Enumerable
+                .Repeat(Tuple.Create(default(char), default(Label), new[] { default(AutomataName) }.ToList()), 0)
+                .ToList();
+            foreach(var charGroup in chars)
             {
-                var ch = chars[0];
+                var ch = charGroup.Key;
+                var items = charGroup.ToList();
                 if (ch == -1)
                 {
-                    d.AddAction(() =>
+                    if (charGroup.Count() != 1)
                     {
-                        d.Emit.MarkLabel(onMatchChar);
-                        nameValues[0].OnFound(d.Emit);
-                        d.Emit.Return();
-                    });
+                        throw new ApplicationException("");
+                    }
+                    var completeName = d.Emit.DefineLabel("_" + items[0].Name + "'complete");
+                    namesToFinish.Add(Tuple.Create('"', completeName, items));
+                    FinishName(d, items[0], completeName);
                 }
                 else
                 {
-                    var next = d.Emit.DefineLabel("_" + nameValues[0].Name.Substring(0, pos + 1));
-                    Recurse(d, nameValues, pos + 1, next);
+                    var next = d.Emit.DefineLabel("_" + items[0].Name.Substring(0, pos + 1));
+                    namesToFinish.Add(Tuple.Create((char)ch, next, items));
+                    NextChar(d, items, pos + 1, next);
+                }
+            }
 
+            d.AddAction(() => d.Emit.Branch(d.Failure));
+
+            Action checkChars = () =>
+            {
+                // TODO: optimize this; use 'switch' and/or binary split depending on number/layout of characters
+                foreach (var item in namesToFinish)
+                {
                     d.AddAction(() =>
                     {
-                        if (onMatchChar != null)
-                            d.Emit.MarkLabel(onMatchChar);
-                        d.Emit.LoadArgument(0);              // TextReader
-                        d.Emit.CallVirtual(TextReader_Read); // char
-                        d.Emit.StoreLocal(d.Local_ch);       //
-
-                        Action checkChar = () =>
-                        {
-                            d.Emit.LoadLocal(d.Local_ch);    // char
-                            d.Emit.LoadConstant(ch);         // char char
-                            d.Emit.BranchIfEqual(next);      //
-                        };
-
-                        checkChar();
-
-                        d.Emit.LoadLocal(d.Local_ch);               // char
-                        d.Emit.LoadConstant('\\');                  // char char
-                        d.Emit.UnsignedBranchIfNotEqual(d.Failure); // 
-                        d.Emit.LoadArgument(0);                     // TextReader
-                        d.Emit.Call(Helper_ExpectUnicodeHexQuad);   // char
-                        d.Emit.StoreLocal(d.Local_ch);              //
-
-                        checkChar();
-
-                        d.Emit.Branch(d.Failure);
+                        d.Emit.LoadLocal(d.Local_ch);
+                        d.Emit.LoadConstant((int)item.Item1);
+                        d.Emit.BranchIfEqual(item.Item2);
                     });
                 }
-            }
-            else if (chars.Count > 1)
-            {
-                var namesToFinish =
-                    Enumerable
-                    .Repeat(Tuple.Create(default(char), default(Label), new[] { nameValues[0] }.ToList()), 0)
-                    .ToList();
-                while (i < nameValues.Count)
-                {
-                    var next = d.Emit.DefineLabel("_" + nameValues[i].Name.Substring(0, pos + 1));
-                    var j = i + 1;
-                    var subset = (new[] { nameValues[i] }).ToList();
-                    var ch = nameValues[i].Name[pos];
-                    while (j < nameValues.Count)
-                    {
-                        if (ch != nameValues[j].Name[pos])
-                            break;
-                        subset.Add(nameValues[j]);
-                        ++j;
-                    }
-                    namesToFinish.Add(Tuple.Create(ch, next, subset));
-                    Recurse(d, subset, pos + 1, next);
-                    i = j;
-                }
-
-                d.AddAction(() => d.Emit.Branch(d.Failure));
-
-                Action checkChars = () =>
-                {
-                    // TODO: optimize this; use 'switch' and/or binary split depending on number/layout of characters
-                    foreach (var item in namesToFinish)
-                    {
-                        d.AddAction(() =>
-                        {
-                            d.Emit.LoadLocal(d.Local_ch);
-                            d.Emit.LoadConstant((int)item.Item1);
-                            d.Emit.BranchIfEqual(item.Item2);
-                        });
-                    }
-                };
+            };
                 
-                checkChars();
+            checkChars();
 
-                d.AddAction(() =>
+            d.AddAction(() =>
+            {
+                d.Emit.LoadLocal(d.Local_ch);               // char
+                d.Emit.LoadConstant('\\');                  // char char
+                d.Emit.UnsignedBranchIfNotEqual(d.Failure); // 
+                d.Emit.LoadArgument(0);                     // TextReader
+                d.Emit.Call(Helper_ExpectUnicodeHexQuad);   // char
+                d.Emit.StoreLocal(d.Local_ch);              //
+            });
+
+            checkChars();
+
+            d.AddAction(() =>
+            {
+                if (onMatchChar != null)
                 {
-                    d.Emit.LoadLocal(d.Local_ch);               // char
-                    d.Emit.LoadConstant('\\');                  // char char
-                    d.Emit.UnsignedBranchIfNotEqual(d.Failure); // 
-                    d.Emit.LoadArgument(0);                     // TextReader
-                    d.Emit.Call(Helper_ExpectUnicodeHexQuad);   // char
-                    d.Emit.StoreLocal(d.Local_ch);              //
-                });
-
-                checkChars();
-
-                d.AddAction(() =>
-                {
-                    if (onMatchChar != null)
-                    {
-                        d.Emit.MarkLabel(onMatchChar);
-                        onMatchChar = null;
-                    }
-                    d.Emit.LoadArgument(0);
-                    d.Emit.CallVirtual(TextReader_Read);
-                    d.Emit.StoreLocal(d.Local_ch);
-                });
-            }
+                    d.Emit.MarkLabel(onMatchChar);
+                    onMatchChar = null;
+                }
+                d.Emit.LoadArgument(0);
+                d.Emit.CallVirtual(TextReader_Read);
+                d.Emit.StoreLocal(d.Local_ch);
+            });
         }
 
         public static AutomataName CreateName(string name, Action<Emit<Func<TextReader, T>>> onFound)
@@ -247,11 +210,11 @@ namespace Jil.Deserialize
             return new AutomataName(name, onFound);
         }
 
-        public static Func<TextReader, T> Create(IList<AutomataName> names, Action<Emit<Func<TextReader, T>>> errorValue)
+        public static Func<TextReader, T> Create(IEnumerable<AutomataName> names, Action<Emit<Func<TextReader, T>>> errorValue)
         {
             var sorted =
                 names
-                .Select(kv => new AutomataName(kv.Name + '\"', kv.OnFound))
+                //.Select(kv => CreateName(kv.Name+'"', kv.OnFound))
                 .OrderBy(kv => kv.Name)
                 .ToList();
 
@@ -275,7 +238,7 @@ namespace Jil.Deserialize
 
             var d = new Data(addAction, emit, failure, ch);
 
-            Recurse(d, sorted, 0, null);
+            NextChar(d, sorted, 0, null);
 
             foreach (var action in stack)
             {
