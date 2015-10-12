67fbe2b4a5f8d7dc27b980ffeeb8b066c1fd22ab
kevin-montrose <kevin.montrose@gmail.com>
2015-10-12 16:17:23 -0400

    missing file

---
+++ Jil/Serialize/UnionMembersNullSigil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jil.Serialize
+{
+    // just used to signal that a union was all null
+    sealed class UnionMembersNullSigil
+    {
+    }
+}
