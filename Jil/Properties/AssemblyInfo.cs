239842325bacde7870acfc6e304872fd1b6e02ec
kevin-montrose <kevin.montrose@gmail.com>
2016-04-13 10:09:18 -0400

    2.14.1

--- Jil/Properties/AssemblyInfo.cs
+++ Jil/Properties/AssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a4bd090c-cd3e-4f8e-af53-a947b0a87316")]
 
-[assembly: AssemblyVersion("2.14.0")]
-[assembly: AssemblyFileVersion("2.14.0")]
+[assembly: AssemblyVersion("2.14.1")]
+[assembly: AssemblyFileVersion("2.14.1")]
 
 #if !STRONG_NAME
 [assembly: InternalsVisibleTo("JilTests")]
