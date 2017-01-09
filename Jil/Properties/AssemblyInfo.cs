032b793a85c3ada86b4e7b01694002beb11178d2
kevin-montrose <kevin.montrose@gmail.com>
2017-01-09 10:21:33 -0500

    update Sigil to 4.7.0

--- Jil/Properties/AssemblyInfo.cs
+++ Jil/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Jil")]
 [assembly: AssemblyDescription("A fast JSON serializer and deserializer")]
 [assembly: AssemblyProduct("Jil")]
-[assembly: AssemblyCopyright("Copyright © Kevin Montrose 2013 - 2016")]
+[assembly: AssemblyCopyright("Copyright © Kevin Montrose 2013 - 2017")]
 
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a4bd090c-cd3e-4f8e-af53-a947b0a87316")]
 
-[assembly: AssemblyVersion("2.14.5")]
-[assembly: AssemblyFileVersion("2.14.5")]
+[assembly: AssemblyVersion("2.15.0")]
+[assembly: AssemblyFileVersion("2.15.0")]
 
 #if !STRONG_NAME
 [assembly: InternalsVisibleTo("JilTests")]
